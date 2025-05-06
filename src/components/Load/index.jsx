b83901056ff8cd355b6b81dbe388c066c9e3cf1c
zzmarques <walacemarqs@gmail.com>
2025-05-06 18:06:46 -0300

    criando componente load

---
+++ src/components/Load/index.jsx
@@ -0,0 +1,10 @@
+
+const Load = () => {
+    return (
+        <div className="container-load">
+            <div className="load"></div>
+        </div>
+    );
+};
+
+export default Load;
--- src/components/SignUpModal/index.jsx
+++ src/components/SignUpModal/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { handleValidator } from "../../utils/formvalidator";
 import { setUser } from "../../utils/userPost";
+import Load from "../Load";
 
 
 
 const SignUpModal = ({ sendState }) => {
 
     const [spanErro, setSpanErro] = useState(false);
+    const [load, setLoad] = useState(false);
 
     const handleValid = (e) => {
         const el = e.target;
@@ -17,27 +19,33 @@ const SignUpModal = ({ sendState }) => {
     const handleUser = () => {
         const nameUser = document.querySelector('#name').value;
         setUser(nameUser);
+        setLoad(true);
         setTimeout(() => {
             sendState(true);
-        }, 2000);
+        }, 2000);   
     }
 
     return (
         <section className="SignUpModal">
-            <h1>Welcome to CodeLeap network!</h1>
+            {load !== true ? <h1>Welcome to CodeLeap network!</h1> : ''}
+            {
+            load ? <Load/> 
+                : 
+                <form 
+                    className="form-sing-up"
+                    onSubmit={(e) => e.preventDefault()}
+                >
+                    <div className="form-group">
+                        <label htmlFor="name">Please enter your username</label>
+                        <input onInput={handleValid} type="text" name="name" id="name" placeholder="John doe"/>
+                        {spanErro && <span className="error">Please enter your name.</span>}
+                    </div>
 
-            <form 
-                className="form-sing-up"
-                onSubmit={(e) => e.preventDefault()}
-            >
-                <div className="form-group">
-                    <label htmlFor="name">Please enter your username</label>
-                    <input onInput={handleValid} type="text" name="name" id="name" placeholder="John doe"/>
-                    {spanErro && <span className="error">Please enter your name.</span>}
-                </div>
-
-                <button onClick={handleUser} className="btn btn-enter" type="submit" >enter</button>
-            </form>
+                    <button onClick={handleUser} className="btn btn-enter" type="submit" >enter</button>
+                </form>
+            }
+            
+            
         </section>
     );
 };
